a57f82f1f8dd5396d1eba3f25f5b6afc1cd31b0e
ilya <ivchvanov@edu.hse.ru>
2025-01-21 23:21:18 +0300

    fix(Controller): move getReadersWithUnreturnedBooks to ReaderController

--- src/main/java/ru/chistoapp/api/controller/AuthorController.java
+++ src/main/java/ru/chistoapp/api/controller/AuthorController.java
@@ -7,28 +7,26 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import ru.chistoapp.api.dto.AuthorDto;
-import ru.chistoapp.api.dto.ReaderDto;
 import ru.chistoapp.api.factory.AuthorDtoFactory;
-import ru.chistoapp.api.factory.ReaderDtoFactory;
 import ru.chistoapp.repository.TransactionRepository;
-import ru.chistoapp.store.*;
+import ru.chistoapp.store.AuthorEntity;
+import ru.chistoapp.store.BookEntity;
+import ru.chistoapp.store.TransactionEntity;
+import ru.chistoapp.store.TransactionType;
 
 import java.time.LocalDate;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 @RestController
 @AllArgsConstructor
 public class AuthorController {
 
     private final AuthorDtoFactory authorDtoFactory;
-    private final ReaderDtoFactory readerDtoFactory;
     private final TransactionRepository transactionRepository;
 
     private final String GET_FAMOUS_AUTHOR = "api/authors/famous";
-    private final String GET_READERS_SORTED_BY_UNRETURNED_BOOKS = "api/authors/readers_with_unreturned_books";
 
     @GetMapping(GET_FAMOUS_AUTHOR)
     @Operation(summary = "Get the most popular author within a date range",
@@ -68,26 +66,4 @@ public class AuthorController {
         return authorDtoFactory.makeAuthorDto(mostPopularAuthor);
     }
 
-    @GetMapping(GET_READERS_SORTED_BY_UNRETURNED_BOOKS)
-    @Operation(summary = "Get readers sorted by the number of unreturned books ",
-            description = "Returns a list of all readers sorted by the number of unreturned books in descending order.")
-    public List<ReaderDto> getReadersWithUnreturnedBooks() {
-
-        List<TransactionEntity> transactions = transactionRepository.findAll();
-        Map<ReaderEntity, Long> unreturnedBooksCount = new HashMap<>();
-        for (TransactionEntity transaction : transactions) {
-            if (transaction.getTransactionType().equals(TransactionType.TAKE)) {
-                ReaderEntity client = transaction.getClient();
-                unreturnedBooksCount.put(client, unreturnedBooksCount.getOrDefault(client, 0L) + 1);
-            } else if (transaction.getTransactionType().equals(TransactionType.RETURN)) {
-                ReaderEntity client = transaction.getClient();
-                unreturnedBooksCount.put(client, unreturnedBooksCount.getOrDefault(client, 0L) - 1);
-            }
-        }
-        return unreturnedBooksCount.entrySet().stream()
-                .sorted((e1, e2) -> Long.compare(e2.getValue(), e1.getValue()))
-                .map(Map.Entry::getKey)
-                .map(readerDtoFactory::createReaderDto)
-                .collect(Collectors.toList());
-    }
 }
